4fc777b5a738420e4619b3e13b63834f030c00de
birbi3 <birbie@protonmail.com>
2019-10-01 21:36:09 -0500

    all binary values are 8 bits so they can be within ascii table values

--- number_gen.py
+++ number_gen.py
@@ -10,21 +10,34 @@ import numpy
 
 
 def main(argv):
-	values = [None] * 10
+	values = [None] * 256
 	dur = int(argv[0])
 	fs = 48000
 	myrecording = sd.rec(dur * fs, blocking=True, channels=2)
 	sd.wait()
 	
-	for i in range(10):
+	for i in range(256):
 		values[i] = randint(0, len(myrecording))
 
 	values = [str(myrecording[_val][0]) for _val in range(len(values))]
 	values = [binary_format(str(values[_row])) for _row in range(len(values))]
 	values = [bin(int(_val))[2:] for _val in values]
-	
+
+	for _row in range(len(values)):
+		curr = _row
+		if len(str(values[_row])) < 8:
+			_row = str(values[_row])
+			padding = 8 - len(values[curr])
+			padding = [str(randint(0,1)) for pad in range(padding)]
+			for pad in padding:
+				values[curr] = values[curr] + pad
+
+		else:
+			values[_row] = (str(values[_row])[:8])
+		
 	for _row in values:
 		print(_row)
+
 
 
 def binary_format(val):
