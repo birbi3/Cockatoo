18ab4617b3fd2885e4dc5e89f6135c162338c78f
birbi3 <birbie@protonmail.com>
2019-12-26 18:32:29 -0600

    generates keys properly!

--- number_gen.py
+++ number_gen.py
@@ -29,54 +29,30 @@ def main(argv, argv2):
 
 	#formats the audio data for binary conversion
 	values = [binary_format(str(values[_row])) for _row in range(len(values))]
-	print("plain ints")
-	print("="*50 + "\n\n")
-	print(values)
 
 	#removes format
 	values = [bin(int(_val))[2:] for _val in values]
 	values = "".join(values)
 	n = 8
 	values = [values[index : index + n] for index in range(0, len(values), n)]
-	print("binary ints")
-	print("="*50 + "\n\n")
-	print(values)
-	print("\n\n")
 
 	for _row in range(0,len(values)):
-		for i in range(100000):
+		for i in range(1000):
 			values[_row] = check_values(values[_row], myrecording)
 
-
-	print("random values")
-	print(values)
-	print('\n\n')
-
-	#splits the values into 8 bits and adds padding if there isn't enough bits
-	for _row in range(len(values)):
-		if len(str(values[_row])) < 7:
-			padding = 7 - len(values[_row])
-			padding = [str(randint(0,1)) for pad in range(padding)]
-			for pad in padding:
-				values[_row] = values[_row] + pad
-
-		else:
-			values[_row] = (str(values[_row])[:7])
-	
 	#converts it to ascii 	
 	values = [chr(int(_row, 2)) for _row in values]
 	key = "".join(values)
-	print(key) 
-
+	print(key)
 
 def check_values(current_value, recording):
 	test_val = BitArray(bin=current_value)
 	if int(test_val.uint) <= 32:
 		new_value = (sub_recording_parse(recording))
-		return new_value
+		return check_values(new_value, recording)
 	elif 127 <= int(test_val.uint):
 		new_value = (sub_recording_parse(recording))
-		return new_value
+		return check_values(new_value, recording)
 	else:
 		return current_value
 
